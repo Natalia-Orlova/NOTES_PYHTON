cbb72874155b33c6336520ba88acf8945f6f46c5
NataliaOrlova <natakarelskaya@yandex.ru>
2023-03-07 20:58:02 +0400

    added new_note, notes_list

---
+++ controller.py
@@ -0,0 +1,32 @@
+import logger as log
+import menu as menu
+import note as note
+import csv
+
+def notes():
+    while True:
+        menu.main_menu()
+        c = input("Enter choice: ")
+        if c == '0':
+            break
+        if c == '1':
+            log.logger("Добавлена новая заметка")
+            note.add_note()
+            print('\nЗаметка успешно сохранена!')
+            # menu.main_menu()
+            # c = input("Enter choice: ")
+            continue
+            
+
+        elif c == '2':
+            log.logger("Открыт список заметок")
+            note.show_notes_list()
+            c = input("Press any key to continue: ")
+            continue
+            
+        
+        else:
+                log.logger("Invalid value entered")
+                print ("Invalid value")
+                continue
+        return
--- note.py
+++ note.py
@@ -1,6 +1,29 @@
-def new_note():
+import csv
+from datetime import datetime as dt
+
+
+# def new_note():
+#     head = input('Введите заголовок заметки: ')
+#     note = input('Введите тело заметки: ')
+#     print(head + '; ' + note)
+
+
+def add_note():
+    time = dt.now().strftime('%D %H:%M')
     head = input('Введите заголовок заметки: ')
     note = input('Введите тело заметки: ')
-    print(head + '; ' + note)
+    with open('notes.csv', 'a', encoding='cp1251') as myFile:
+        myFile.write('{}; {}; {}\n'
+                    .format(head, note, time))
 
-new_note()
+
+# def save_note():
+
+
+def show_notes_list():
+    with open('notes.csv', 'r', encoding='cp1251') as file:
+        read_file = file.read()
+    print(read_file)
+
+# add_note()
+# show_notes_list()
